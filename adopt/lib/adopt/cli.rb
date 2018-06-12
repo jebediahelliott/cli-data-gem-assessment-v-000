0bbfaf098011d6ca2bc3f2ac654318beec141ed6
Jebediah Elliott <jebediahelliott@gmail.com>
2018-06-12 00:18:32 +0000

    Done.

---
+++ adopt/lib/adopt/cli.rb
@@ -0,0 +1,36 @@
+class DogAdoptionCli::CLI
+
+  def call
+    list
+    menu
+  end
+
+  def list
+    puts "Welcome! Here is a list of dogs available for adoption at Centre County Paws"
+    puts "1. Ballpark Frank: Male, Coonhound, Adult, Large"
+    puts "2. Beaufort: Male, Beagle/Terrier, Adult, Small"
+    puts "3. Brandi: Female, Labrador Retriever/Husky, Senior, Large"
+  end
+
+  def menu
+
+    input = nil
+    while input != "exit"
+      puts "Please choose a dogs number to see more information. Type list to return to list or type exit."
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "Ballpark Frank..."
+      when "2"
+        puts "Beaufort..."
+      when "3"
+        puts "Brandi..."
+      end
+    end
+    goodbye
+  end
+
+  def goodbye
+    puts "Thanks for checking out our furry friends!"
+  end
+end
